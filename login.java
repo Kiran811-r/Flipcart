e8ebab2c3e91db91e4d10926bb62a456795ba4d1
kiran811-r <kirankumarr836@gmail.com>
2024-07-08 22:29:56 +0530

    merge succefully created

--- login.java
+++ login.java
@@ -1,12 +1,12 @@
 1  user{ flipcart new project}
 2 print
 3 html
-4 user print flipcart work
+
 5 script
 6 login.java
 7 print [ login.java in flipcart ]
-8 ok a=1
-9  a=2
-10 a=3
+8 ok a=10
+
+
 
 
